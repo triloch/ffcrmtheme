e3e41354b22f1386852a9c768d85c52396d238e3
pani <panigrahi@zoho.com>
2016-09-27 22:09:25 +0800

    Initial Commit

---
+++ lib/ffcrmtheme.rb
@@ -0,0 +1,6 @@
+require "ffcrmtheme/version"
+
+module Ffcrmtheme
+  # Your code goes here...
+  class Engine < ::Rails::Engine; end
+end
---
+++ lib/ffcrmtheme/version.rb
@@ -0,0 +1,3 @@
+module Ffcrmtheme
+  VERSION = "0.1.0"
+end
